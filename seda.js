381c0ffd3f2aec9435928d919c12bb7f84cd5c9a
aescherling <adam.scherling@gmail.com>
2017-12-20 08:54:12 -0800

    added variable selector

--- seda.js
+++ seda.js
@@ -1,4 +1,8 @@
 
+// globally accessible data variables, primarily for debugging
+var myData;
+var myVar;
+
 // create an svg - everything will be drawn on this
 var svg = d3.select('#viz').append('svg')
 	.attr('height', '400px')
@@ -31,7 +35,7 @@ histogram.append('g')
 // function for calculating the counts necessary for producing a histogram
 // input: data, lower end of lowest bin, high end of highest bin, desired # of bins
 // output: JS object with variable "value" = count for each bin
-calcHist = function (x, low, high, bins) {
+function calcHist(x, low, high, bins) {
   // scale the data & take floor to collapse into bins
   histScale = d3.scaleLinear().domain([low, high]).range([0,bins]);
   scaled = x.map(function(a) {return Math.floor(histScale(a));});
@@ -48,43 +52,21 @@ calcHist = function (x, low, high, bins) {
 }
 
 
-// globally accessible data variables, primarily for debugging
-var myData;
-var myVar;
+// function for updating the histogram with input variable (array) x
+function updateHist(x) {
 
-
-// when the data have loaded, explore!
-queue()
-  .defer(d3.csv, 'myData.csv')
-  .await(explore)
-
-
-// d3.csv("myData.csv", function(data) {
-// 	myData = data;
-// });
-
-// function to plot the data
-function explore(error, data) {
-	if (error) throw error;
-
-	// create crossfilter
-	var cf = crossfilter(data);
-
-	// for the test version, parse y variable from the test data
-	// in the actual version, the variable will be selected by the user
-	var y = data.map(function(d) {return d.y});
-	var yMin = d3.min(y);
-	var yMax = d3.max(y);
+	var xMin = d3.min(x);
+	var xMax = d3.max(x);
 
 	// calculate the counts of the chosen variable using calcHist
 	var bins = 10;
-	var counts = calcHist(y, Math.floor(yMin), Math.ceil(yMax), bins)
-	var stepSize = (Math.ceil(yMax) - Math.floor(yMin)) / bins;
+	var counts = calcHist(x, Math.floor(xMin), Math.ceil(xMax), bins)
+	var stepSize = (Math.ceil(xMax) - Math.floor(xMin)) / bins;
 	myVar = counts;
 
 	// rescale/relabel x axis
-	histScaleX = d3.scaleLinear().domain([Math.floor(yMin), Math.ceil(yMax)]).range([0, histWidth]);
-	var barWidth = histScaleX(Math.floor(yMin) + stepSize);
+	histScaleX = d3.scaleLinear().domain([Math.floor(xMin), Math.ceil(xMax)]).range([0, histWidth]);
+	var barWidth = histScaleX(Math.floor(xMin) + stepSize);
 	d3.select('.xAxis').remove();
 	histogram.append('g')
   		.attr('class', 'axis xAxis')
@@ -114,9 +96,112 @@ function explore(error, data) {
 	// update bars
     d3.selectAll('.bar')
       .transition()
-      .duration(200)
+      .duration(100)
       .attr('height', function (d,i) {return histHeight - histScaleY(counts[i])})
       .attr('y', function (d,i) {return histScaleY(counts[i])});
+
+} // end of updateHist()
+
+
+
+// function for creating a variable selector
+function makeSelector(id, data, variables) {
+	// if a filter with this id already exists, remove it
+	d3.selectAll('#' + id).remove();
+
+	// set dimensions
+    var width = 200;
+    var height = 20;
+    var textwidth = 55;
+    var margin = 5;
+
+	// create the scales
+    var x = d3.scaleLinear()
+    	.domain([0, variables.length-1])
+    	.range([0, width - 2*margin]);
+
+    var y = d3.scaleLinear()
+    	.domain([0, variables.length-1])
+    	.range([height/2, height/2]);
+
+    // a line based on the scales
+    var line = d3.line()
+        .x(function(d, i) { return x(i); })
+        .y(function(d, i) { return y(i); });
+
+    // create a selector group
+	var selector = svg.append('g')
+		.attr('id', id)
+		.attr('transform', 'translate(105,350)');
+
+	// add the line to the selector
+	var path = selector.append('path')
+      .datum(variables)
+      .attr('d', line)
+      .attr('style', 'fill: none; stroke: #000; stroke-width: 0.5px;');
+
+    // dot shows the current time selection
+    var dot = selector.append("circle")
+        .attr('cx', x(0))
+        .attr('cy', y(0))
+        .attr("r", 5);
+
+    // variable name
+    var varName = selector.append("text")
+    	.attr("x", 100)
+    	.attr("y", -10)
+    	.attr("text-anchor", "middle")
+    	.attr("text", "");
+
+    function mousemove() {
+    	var i = Math.min(Math.round(x.invert(d3.mouse(this)[0])), variables.length-1);
+    	dot.attr('cx', x(i)).attr('cy', y(i));
+    	varName.text(variables[i]);
+
+    	// update the histogram to view the selected variable
+    	var newVar = data.map(function(d) {return d[variables[i]]});
+		updateHist(newVar);
+    }
+
+    selector.append("rect")
+        .attr("class", "overlay")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("style", "fill: none; pointer-events: all;")
+        .on("mousemove", mousemove);
+
+
+	// selector.append('rect').attr('x',0).attr('y',0).attr('width',10).attr('height',10).attr('fill','black');
+
+
+} // end of makeSelector();
+
+
+
+// when the data have loaded, explore!
+queue()
+  .defer(d3.csv, 'myData.csv')
+  .await(explore);
+
+
+// function to plot the data
+function explore(error, data) {
+	if (error) throw error;
+
+	// get a list of the variable names
+	var varNames = Object.keys(data[0]);
+
+	// create crossfilter
+	var cf = crossfilter(data);
+
+	// default to selecting the first column of the csv
+	// later the user will be able to try all the different variables
+	var x = data.map(function(d) {return d[varNames[0]]});
+	updateHist(x);
+
+    // create a toolbar for switching the variable
+    var selector = makeSelector('selector1', data, varNames);
+
 
 } // end of explore
 
